4911b03e771d96a0fe4bd58bc44df08d0a9ad2d6
Bakuyto <lyhengoeur@gmail.com>
2024-03-04 16:25:44 +0700

    Update config.php

--- Project/config/config.php
+++ Project/config/config.php
@@ -60,16 +60,13 @@ if(isset($_POST['submit_user'])) {
 if(isset($_POST['submit_delete_transaction']) && isset($_POST['transactionToDelete']) && !empty($_POST['transactionToDelete'])) {
   $transaction_value = $conn->real_escape_string($_POST['transactionToDelete']);
   $sql = "CALL Delete_Column('tblproduct_transaction','$transaction_value')";
-  $sql1 = "DELETE FROM tblproductadjustpermission WHERE product_tran_name_str = '$transaction_value'";
 
-  if ($conn->query($sql) === TRUE && $conn->query($sql1) === TRUE) {
+  if ($conn->query($sql) === TRUE) {
       echo "<script>alert('Transaction data deleted successfully');</script>";
       // Redirect to avoid form resubmission or reload the current page
       header("Location: ".$_SERVER['REQUEST_URI']);
       exit();
-  } else {
-      echo "Error: " . $conn->error;
-  }
+  } 
 }
 if(isset($_POST['save_multiple_checkbox'])) {
   $department_fk = $_POST['department_fk']; // Get the department_pk from the form
